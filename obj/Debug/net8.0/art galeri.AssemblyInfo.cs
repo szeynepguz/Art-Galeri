ebb60057236968954e2fef065433cd2609cca933
lara cihan <laracihann1@gmail.com>
2026-05-12 02:19:56 +0300

    feat: add artwork listing view with search, filter, and sorting functionality

--- obj/Debug/net8.0/art galeri.AssemblyInfo.cs
+++ obj/Debug/net8.0/art galeri.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("art galeri")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91ffbb9c47525e362d8833a48e58b30753514bd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a586dd6e740740dc3eb1c13de83d2734a75451a")]
 [assembly: System.Reflection.AssemblyProductAttribute("art galeri")]
 [assembly: System.Reflection.AssemblyTitleAttribute("art galeri")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
